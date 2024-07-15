db9bbb080584267e4066ae8ae7940454078930d5
Tamás Vince Kornél <vin@kys.social>
2024-07-15 02:31:08 +0200

    nicer posts admin UI

--- src/routes/admin/posts/+page.server.ts
+++ src/routes/admin/posts/+page.server.ts
@@ -1,12 +1,19 @@
 import { error, type HttpError } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import { prisma } from '$lib/server/db';
+import { lucia } from '$lib/server/auth';
+import { fail } from '@sveltejs/kit';
+import { minio } from '$lib/server/minio';
+import { MINIO_BUCKET } from '$env/static/private';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async (event) => {
   try {
     const posts = await prisma.post.findMany({
       orderBy: {
         createdAt: 'asc'
+      },
+      where: {
+        authorId: event.locals.user?.id
       }
     });
 
@@ -20,3 +27,92 @@ export const load: PageServerLoad = async ({ params }) => {
     return error(500, { message: 'Internal Server Error' });
   }
 };
+
+export const actions = {
+  delete: async ({ request, cookies }) => {
+    const sessionId = cookies.get(lucia.sessionCookieName);
+    if (!sessionId) {
+      return fail(401, { error: 'No session found' });
+    }
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (!session || !user || !user.isSiteAdmin) {
+      return fail(401, { error: 'No permission' });
+    }
+    const data = await request.formData();
+
+    if (!data.has("id")) {
+      return fail(404, { error: 'No post ID' });
+    }
+
+    const postId = data.get("id") as string
+    const post = await prisma.post.findUnique({ where: { id: postId } })
+
+    if (!post) {
+      return fail(404, { error: "Post not found" })
+    }
+
+    const files = post?.fileIDs
+    if (files.length > 0) {
+      try {
+        minio.removeObjects(MINIO_BUCKET, files)
+      } catch (e) {
+        return fail(500, { error: 'Internal Server Error' });
+      }
+    }
+
+    try {
+      await prisma.post.delete({
+        where: {
+          id: postId
+        }
+      })
+    } catch (e) {
+      return fail(500, { error: "Database error" })
+    }
+
+    return {
+      status: 200
+    }
+  },
+  changePublish: async ({request, cookies}) => {
+    const sessionId = cookies.get(lucia.sessionCookieName);
+    if (!sessionId) {
+      return fail(401, { error: 'No session found' });
+    }
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (!session || !user || !user.isSiteAdmin) {
+      return fail(401, { error: 'No permission' });
+    }
+    const data = await request.formData();
+
+    if (!data.has("id")) {
+      return fail(404, { error: 'No post ID' });
+    }
+
+    const postId = data.get("id") as string
+    const post = await prisma.post.findUnique({ where: { id: postId } })
+
+    if (!post) {
+      return fail(404, { error: "Post not found" })
+    }
+    try {
+      await prisma.post.update({
+        where: {
+          id: post.id
+        },
+        data: {
+          published: !post.published
+        }
+      })
+    } catch (e) {
+      return fail(500, { error: "Database error" })
+    }
+
+    console.log("changed post publish to " + post.published);
+    
+    return {
+      status: 200,
+      body: post
+    }
+  }
+}
--- src/routes/admin/posts/+page.svelte
+++ src/routes/admin/posts/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte';
+  import { enhance } from '$app/forms';
+  import { goto, invalidateAll } from '$app/navigation';
+  import { toast } from 'svelte-sonner';
   export let data;
 </script>
 
@@ -7,7 +11,82 @@
     {@const san = post.content?.replace(/<[^>]*>/g, '')}
     <div class="card lg:card-side bg-base-200 shadow-xl">
       <div class="card-body">
-        <h2 class="card-title"><a href="/admin/post/edit/{post.id}"> {post.title}</a></h2>
+        <div class="flex justify-between">
+          <h2 class="card-title">{post.title}</h2>
+          <div class="flex gap-2">
+            <form
+              method="post"
+              action="?/changePublish"
+              use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+                formData.append('id', post.id);
+
+                return async ({ result }) => {
+                  if (result.type === 'success') {
+                    toast.success(`Post ${!post.published ? 'published' : 'unpublished'}!`);
+                    invalidateAll();
+                  } else if (result.type === 'failure') {
+                    toast.error('Failed to update post');
+                  }
+                };
+              }}
+            >
+              <div
+                class="tooltip"
+                class:tooltip-primary={!post.published}
+                class:tooltip-warning={post.published}
+                data-tip={post.published ? 'Unpublish' : 'Publish'}
+              >
+                <button
+                  type="submit"
+                  class="btn btn-outline btn-square btn-sm"
+                  class:btn-primary={!post.published}
+                  class:btn-warning={post.published}
+                >
+                  <Icon
+                    icon={post.published
+                      ? 'material-symbols:unpublished'
+                      : 'material-symbols:publish'}
+                    class="text-lg"
+                  />
+                </button>
+              </div>
+            </form>
+
+            <div class="tooltip tooltip-accent" data-tip="Edit">
+              <button
+                class="btn btn-outline btn-accent btn-square btn-sm"
+                on:click={() => {
+                  goto(`/admin/post/edit/${post.id}`);
+                }}
+              >
+                <Icon icon="mdi:edit" class="text-lg" />
+              </button>
+            </div>
+
+            <form
+              method="post"
+              action="?/delete"
+              use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+                formData.append('id', post.id);
+
+                return async ({ result }) => {
+                  if (result.type === 'success') {
+                    toast.success(`Post deleted!`);
+                    invalidateAll();
+                  } else if (result.type === 'failure') {
+                    toast.error('Failed to update post');
+                  }
+                };
+              }}
+            >
+              <div class="tooltip tooltip-error" data-tip="Delete">
+                <button class="btn btn-outline btn-error btn-square btn-sm">
+                  <Icon icon="mdi:delete" class="text-lg" />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
         <div class="flex gap-3">
           {#if post.published}
             <div class="badge badge-primary">Published</div>
